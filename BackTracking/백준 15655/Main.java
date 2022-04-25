cacd851b469ddff430dcaec51692c9116dd1ae2a
young2866 <thfkdudrhkd@gmail.com>
2022-04-25 18:25:00 +0900

    BackTracking

---
+++ BackTracking/백준 15655/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+
+
+public class Main {
+    static int[] arr;
+    static int[] num;
+    static boolean[] chk;
+
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        num = new int[N];
+        arr = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
+        }
+        Arrays.sort(num);
+
+        Back(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void Back(int a,int idx) {
+        if (idx == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        for (int i = a; i < N; i++) {
+            arr[idx] = num[i];
+            Back(i + 1, idx + 1);
+        }
+
+    }
+
+
+
+
+
+}
